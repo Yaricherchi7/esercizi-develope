eb99c93e53c28d1516539d8a50937b26625984a8
Yari <yari.cherchi7@gmail.com>
2022-11-07 10:29:04 +0100

    esercizio javascript

---
+++ Javascript/es-10/exercise-7.js
@@ -0,0 +1,21 @@
+function sumUntil(maxValue) {
+  let sum = 0;
+  for (i= 0 ; i < 6 ; i++)
+  sum += i;
+  return sum;
+}
+  
+
+console.log(sumUntil(5));
+
+
+// Create a function `sumUntil` that takes in a parameter and retunrs the
+//  sum of all numbers ranging from one to the value passed as parameter.
+
+// Example:
+
+// function sumUntil(maxValue) {
+// // ...
+// }
+
+// console.log(sumUntil(5)); // OUTPUT: 15
