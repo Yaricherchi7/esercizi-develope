5b95aaea8398c47aed7f6df7a6fb7a96e3cbb0f5
Yari <yari.cherchi7@gmail.com>
2022-11-14 13:34:07 +0100

    esercizio destructuring 4 svolto

---
+++ Javascript/destructuring-4/exercise-5.js
@@ -0,0 +1,13 @@
+const person = {
+  id: 1,
+  firstName: 'Mario',
+  lastName: 'Rossi',
+  age: 25
+};
+
+
+
+const { id,  firstName : name, lastName : surname, age : old } = person;
+
+
+console.log(id, name, surname, old);
