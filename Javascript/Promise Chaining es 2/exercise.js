e2b1411f2db6d3d1958bc86a6db33f4738928918
Yari Cherchi <yari.cherchi7@gmail.com>
2022-11-22 10:17:51 +0100

    esercizio promise chaining svolto

---
+++ Javascript/Promise Chaining es 2/exercise.js
@@ -0,0 +1,26 @@
+const isLogged = true;
+
+function loggedPromise(data) {
+  return new Promise((resolve, reject) => {
+    if (data) {
+      resolve(Math.random());
+    } else {
+      reject(new Error("offline"));
+    }
+  });
+}
+
+function namePromise(nameID) {
+  return new Promise((resolve, reject) => {
+    if (nameID > 0.5) {
+      resolve({ name: "John", age: 24 });
+    } else {
+      reject(new Error("error"));
+    }
+  });
+}
+
+loggedPromise(isLogged)
+  .then(namePromise)
+  .then((run) => console.log(run))
+  .catch((err) => console.log(err))
