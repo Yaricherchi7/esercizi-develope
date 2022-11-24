44e422a7658089cac48b851925202c73d56b54ff
Yari Cherchi <yari.cherchi7@gmail.com>
2022-11-24 13:21:19 +0100

    aggiornato es

--- Javascript/Exercise 67 - Rest parameters and spread syntax/exercise-4.js
+++ Javascript/Exercise 67 - Rest parameters and spread syntax/exercise-4.js
@@ -1,7 +1,7 @@
 function sum(...somma)  {
-  return somma.reduce((a,b)=> a + b )
+  return somma.reduce((a,b)=> a + b );
 }
 
-
+let prova = sum(1,2,3,4)
 /* const numbers = [1, 2, 3]; */
-console.log(sum(1,2,3,4));
+console.log(prova);
